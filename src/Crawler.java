5491999b8a2283bd82d383a318f7ef8c1fd2d4f8
Lukáš Rešutík <lresutik@gmail.com>
2017-10-12 15:27:20 +0200

    Add crawling page
    Add crawling page

--- src/Crawler.java
+++ src/Crawler.java
@@ -5,7 +5,7 @@ import java.util.Set;
 
 public class Crawler {
 
-	private static final int MAX_PAGES_TO_SEARCH = 10;
+	private static final int MAX_PAGES_TO_SEARCH = 20;
 	private Set<String> pagesVisited = new HashSet<String>();
     private List<String> pagesToVisit = new LinkedList<String>();
 	
